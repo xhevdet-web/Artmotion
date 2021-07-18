84abccc1ffc805df2dc64615918ca9c2f5599413
dmaloku <d.maloku@head.wtf>
2021-07-19 00:52:13 +0200

    page 404 and general layout fix

--- wp-content/themes/skynet/404.php
+++ wp-content/themes/skynet/404.php
@@ -10,51 +10,12 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'skynet' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'skynet' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'skynet' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$skynet_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'skynet' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$skynet_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+    <main class="content_wrapper">
+        <div class="wrapper_404">
+            <h1>404</h1>
+            <h3><?php the_field('page_404_message','option');?></h3>
+        </div>
+    </main>
 
 <?php
 get_footer();
