6d8cb53741fde34351791ce3c7d2234df096b639
dmaloku <d.maloku@head.wtf>
2021-07-19 23:58:15 +0200

    added faq layout and some other things

--- wp-content/themes/skynet/custom-fields/layouts/clients_services_layout.php
+++ wp-content/themes/skynet/custom-fields/layouts/clients_services_layout.php
@@ -1,18 +1,16 @@
 <section class="clients_services_layout">
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
-                <div class="box">
-                    <h3>Privat</h3>
-                    <p>Skynet ofron sherbime te internetit me shpejtesi &nbsp;prej 10 mbps, 20 mbps deri ne 50 mbps. Wireless dhe modem falas per kontrata 1 vjeqare, gjithashtu edhe per Pako Duo.</p>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="box">
-                    <h3>Biznes</h3>
-                    <p>Skynet ofron sherbime te internetit me shpejtesi &nbsp;prej 10 mbps, 20 mbps deri ne 50 mbps. Wireless dhe modem falas per kontrata 1 vjeqare, gjithashtu edhe per Pako Duo.</p>
-                </div>
-            </div>
+            <?php if (have_rows('boxes')): ?>
+                <?php while (have_rows('boxes')) : the_row(); ?>
+                    <div class="col-md-6">
+                        <div class="box">
+                            <h3><?php the_sub_field('title');?></h3>
+                            <?php the_sub_field('content');?>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </div>
     </div>
 </section>
---
+++ wp-content/themes/skynet/custom-fields/layouts/faq_layout.php
@@ -0,0 +1,31 @@
+<section class="faq_layout">
+    <div class="container">
+        <div class="faq_layout_title_wrapper">
+            <h2><?php the_sub_field('title');?></h2>
+            <p>
+                <?php the_sub_field('content');?>
+            </p>
+        </div>
+
+        <div class="skynet_accordions">
+            <?php if (have_rows('accordions')): ?>
+                <?php while (have_rows('accordions')) : the_row(); ?>
+                    <div class="skynet_accordion">
+                        <div class="skynet_accordion_header open_accordion">
+                            <h4><?php the_sub_field('title');?></h4>
+                            <span><i class="fa fa-chevron-down"></i></span>
+                        </div>
+                        <div class="skynet_accordion_body">
+                            <div class="skynet_accordion_body_content">
+                                <?php the_sub_field('content');?>
+                                <div class="accordion_image_box">
+                                    <img src="<?php the_sub_field('image'); ?>" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
